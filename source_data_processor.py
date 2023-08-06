5f967d149867bc9505d37f74f2e5743a4788b908
Jimmy Lee <realjimmycycle@gmail.com>
2023-08-06 17:11:05 +0800

    update todo plan of data processor

--- source_data_processor.py
+++ source_data_processor.py
@@ -14,5 +14,22 @@ len(pharmacies_json_data)
 len(users_json_data)
 
 
+def date_time_converter(date_info_string):
+    # remove
+    tmp = date_info_string.strip()
+    tmp = tmp.replace(' ', '')
+    business_hour_list = tmp.split('/')
+
+    for business_hour in business_hour_list:
+        day_of_week_info = business_hour[:-11] # 取出所有的星期幾資訊
+        time_info = business_hour[-11:] # 取出 open, close 時間資訊
+
+        # *** todo ***
+
+    new_time_info = ''
+    return new_time_info
+
 def insert_into_database(data):
+    
+    
     return
